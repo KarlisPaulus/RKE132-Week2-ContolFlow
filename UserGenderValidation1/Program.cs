d0f09da294a127f08abc78da010c983aa277410f
Karlis Paulus <pauluskarlis@gmail.com>
2024-02-06 09:25:07 +0200

    add Usergendervalidation code

---
+++ UserGenderValidation1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //Loeb konsoolist maha andmeid string formaadis
+Console.WriteLine("Please enter your last name:");
+
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr.{userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms.{userLastName}!");
+}
+
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+
+}
